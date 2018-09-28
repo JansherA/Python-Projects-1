f4c86481fed081541dccc748c2cfb2ed29b1b6c4
JansherA <43053017+JansherA@users.noreply.github.com>
2018-09-28 14:14:24 -0400

    Create 28homework.py

---
+++ 28homework.py
@@ -0,0 +1,30 @@
+import random
+
+#1st
+#list1 = []
+#a = range(0,100)
+
+
+#for a in range(0,100):
+#	list1.append(a)
+#	x = list(filter(lambda x: (x % 3 == 0), list1))
+#	list1.remove(x)
+#	print(list1)
+
+
+#1st or 2nd
+list2 = []
+b = range(0,100)
+list2.append(b)
+for b in range(0,100):
+	print(b)
+
+
+#6th
+list3 = []
+d = random.randint(0,10)
+print(a)
+list3.append(d)
+if d % a(1) == 0:
+	list3.remove(d)
+	print(list3)
